fea01d33f1213a369fc30ed42c9853ca7c990803
magikh0e <35289504+magikh0e@users.noreply.github.com>
2026-05-30 07:49:16 -1000

    v0.7.0: time_format constant

--- custom_components/medication_reminder/const.py
+++ custom_components/medication_reminder/const.py
@@ -11,11 +11,13 @@ CONF_NOTIFY = "notify"
 CONF_RESET_TIME = "reset_time"
 CONF_NAG_MINUTES = "nag_minutes"
 CONF_NAG_INTERVAL = "nag_interval"
+CONF_TIME_FORMAT = "time_format"
 
 DEFAULT_PATIENT_TYPE = "person"
 DEFAULT_RESET_TIME = "00:01:00"
 DEFAULT_NAG_MINUTES = 45
 DEFAULT_NAG_INTERVAL = 15
+DEFAULT_TIME_FORMAT = "12h"
 
 # Icon for the patient-level "all doses given" sensor, by patient type.
 PATIENT_ICONS = {
