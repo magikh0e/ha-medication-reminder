51ee418ccaa25f4740083c84110872b1b0adee31
magikh0e <35289504+magikh0e@users.noreply.github.com>
2026-05-29 17:39:17 -1000

    v0.3.0: add reset-time + nag settings constants

--- custom_components/medication_reminder/const.py
+++ custom_components/medication_reminder/const.py
@@ -7,3 +7,10 @@ CONF_DOSES = "doses"
 CONF_TIME = "time"
 CONF_MEDS = "meds"
 CONF_NOTIFY = "notify"
+CONF_RESET_TIME = "reset_time"
+CONF_NAG_MINUTES = "nag_minutes"
+CONF_NAG_INTERVAL = "nag_interval"
+
+DEFAULT_RESET_TIME = "00:01:00"
+DEFAULT_NAG_MINUTES = 45
+DEFAULT_NAG_INTERVAL = 15
